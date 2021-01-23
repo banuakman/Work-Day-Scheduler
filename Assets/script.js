53707bd318d00ef38ab0af9e6efbbad2d20c6e15
banuakman <banu@asterya.com>
2021-01-22 20:36:52 -0500

    add pseudocode

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,30 @@
+// DEPENDENCIES ====================================================
+// // DOM elements that you need to connect
+
+
+// DATA ============================================================
+// //global variable? starting data?
+var curTime = moment();
+var workingHours = 10;
+var startHour = 8
+
+// FUNCTIONS =======================================================
+
 // Add current date & time to the top of the page
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY,  h:mm a"));
+$("#currentDay").text(curTime.format("dddd, MMMM Do YYYY,  h:mm a"));
+
+// generate all the time rows startHour + workingHours
+    // Create // Build // Place
+    // label
+    // text area
+    // save button
+    
+// change color class according to curTime 
+
+// USER INTERRACTIONS ==============================================
+
+// On click to Save Button Save to Local Storage
+
+
+
+
